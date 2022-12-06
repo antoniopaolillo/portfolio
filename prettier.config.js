223a2ee8c6ee639d3e91689b3adfae40b0726a3b
antoniopaolillo <antonio_francisco_p@live.com>
2022-12-06 19:39:57 -0300

    feat: add basic config

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+/* eslint-env node */
+
+/**
+ * Configurações do Prettier.
+ * @type {import('prettier').Options}
+ */
+const configuration = {
+  trailingComma: 'all',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+};
+
+module.exports = configuration;
